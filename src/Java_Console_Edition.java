911208e9950f90dc456d5a626b2b9fdd74990109
Anthony <35087098+agrusk@users.noreply.github.com>
2018-06-30 21:41:19 -0400

    It should be working now

--- src/Java_Console_Edition.java
+++
@@ -1,8 +0,0 @@
-
-public class Java_Console_Edition {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		system.out.println("sfgfes");
-	}
-}
---
+++ src/Text_Version.java
@@ -0,0 +1,4 @@
+
+public class Text_Version {
+
+}
