cb29dccde48efffcdcf9a0f066bc4051fa3b721f
Anthony <35087098+agrusk@users.noreply.github.com>
2018-07-28 20:03:50 -0400

    Removed Scrum

--- src/Board.java
+++ src/Board.java
@@ -7,7 +7,7 @@ public class Board {
 		 * 	I Dont know whats happening in this m ethod but its private tthis is crazy a private constructor? in sane!!
 		 */
 	}
-	
+	 
 	public Board GetInstance() {
 		if (Game == null) {
 			Game = new Board();
