3f8cf3608389d9d9db3fd7fd3714e59e146aeb58
Anthony <35087098+agrusk@users.noreply.github.com>
2018-08-25 12:56:05 -0400

    Quick add

--- src/Board.java
+++ src/Board.java
@@ -28,7 +28,7 @@ public class Board {
 	public Board(int m, int n) {
 		Random rn = new Random();
 		movelist = new ArrayList<Cell>();
-		sm = new Strat_Chain(); //Change this to test, we can work on a better way do it later
+		sm = new Strat_Click(); //Change this to test, we can work on a better way do it later
 		grid = new Cell[m][n];
 		mci = new MoveCommandInvoker();
 		for (int i =0; i<m; i++) {
