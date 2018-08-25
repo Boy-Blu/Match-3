4a4df4e2246fa23de14f725403d3394aff6a0cae
Anthony <35087098+agrusk@users.noreply.github.com>
2018-08-25 16:30:15 -0400

    For mvc

--- src/Text_Version.java
+++ src/Text_Version.java
@@ -25,10 +25,10 @@ public class Text_Version {
 				n=0;
 			}
 		}
-		
+
 		Board game =  new Board(m,n);
 		System.out.println(game);
-		
+
 		while(true) {
 			try {
 				tokens = in.readLine().split(" ");
@@ -40,20 +40,52 @@ public class Text_Version {
 			} catch (NumberFormatException v) {
 			}
 			catch (java.lang.ArrayIndexOutOfBoundsException z) {
-			
+
 			}
-			
+
 			game.move(n, m);	// These are flipped!!!! 
 
 			System.out.println(game);
-			
+
 			// get move
 			if(!game.checkForMove()){
 				System.out.println("\nNo more moves.");
 				return;
 			}
 		}
-		
+
+	}
+	public void run(Board game) {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(game);
+		String[] tokens;
+		int m=0;
+		int n=0;
+		while(true) {
+			try {
+				tokens = in.readLine().split(" ");
+				m = Integer.parseInt(tokens[0]);
+				n = Integer.parseInt(tokens[1]);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (NumberFormatException v) {
+			}
+			catch (java.lang.ArrayIndexOutOfBoundsException z) {
+
+			}
+
+			game.move(n, m);	// These are flipped!!!! 
+
+			System.out.println(game);
+
+			// get move
+			if(!game.checkForMove()){
+				System.out.println("\nNo more moves.");
+				return;
+			}
+		}
+
 	}
 
 
