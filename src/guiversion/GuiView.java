e5a64161e48d6a8ebb78a43a07905a514c50ff67
Anthony <35087098+agrusk@users.noreply.github.com>
2018-09-05 20:28:32 -0400

    Implemented Gui, There's a Jframe Now

--- src/guiversion/GuiView.java
+++ src/guiversion/GuiView.java
@@ -1,14 +1,16 @@
 package guiversion;
 
+import javax.swing.JFrame;
+
 import gamemodel.Board;
 
-public class GuiView {
+public class GuiView extends JFrame{
 	Board game;
 	public GuiView(Board b) {
 
 		this.game = b;
 		
-	
+		this.setVisible(true);
 	}
 
 }
