7b879367c779d206345e113ce09fadb0cc9f3499
omric <37829365+ohcu@users.noreply.github.com>
2018-09-16 19:32:10 -0400

    - Added blank ShapeFactory

---
+++ src/guiversion/ShapeFactory.java
@@ -0,0 +1,25 @@
+package guiversion;
+
+import java.awt.Color;
+import java.awt.Shape;
+
+public class ShapeFactory {
+	/**
+	 * Generates a color based of a input between 1-7 
+	 * @param i the value of colour
+	 * @return A shape
+	 */
+	public static Shape makeShape(int i){
+		switch(i){
+		case 1: return null;
+		case 2: return null;
+		case 3: return null;
+		case 4: return null;
+		case 5: return null;
+		case 6: return null;
+		case 7: return null;
+		default: return null;
+		}
+	}
+
+}
